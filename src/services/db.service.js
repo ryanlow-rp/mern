9b495f7ed01f82f5c09ede2d73aac1b2e21cb16b
Ryan Low <ryan_low@rp.edu.sg>
2023-06-21 10:29:46 +0800

    initial commit

--- src/services/db.service.js
+++ src/services/db.service.js
@@ -3,7 +3,9 @@ const dbConfig = require('../configs/db.config')
 
 async function query(sql, params) {
     const connection = await mysql.createConnection(dbConfig)
-    const [results] = await connection.execute(sql, params)
+    // https://github.com/sidorares/node-mysql2/issues/1239
+    // const [results] = await connection.execute(sql, params)
+    const [results] = await connection.query(sql, params)
 
     return results
 }
